aa45857662f3d1f620d2dca554461e2054639666
Daniel Pietrucha <d.pietrucha@gmi.pl>
2018-10-26 09:38:07 +0200

    Add firebase ngrx component route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HeroesComponent} from './heroes/heroes.component';
 import {NgrxReadCreateComponent} from './ngrx-read-create/ngrx-read-create.component';
+import {FirebaseNgrxComponent} from './firebase-ngrx/firebase-ngrx.component';
 
 const routes: Routes = [
   {path: 'ngrx', component: NgrxReadCreateComponent},
-  {path: 'heroes', component: HeroesComponent}
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'firebasengrx', component: FirebaseNgrxComponent}
 ];
 
 @NgModule({
